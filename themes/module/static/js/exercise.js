fd4ac5dfc5278e6cb50453d7c741c246ccdae3b6
Graeme Stuart <ggstuart@gmail.com>
2023-06-19 17:56:30 +0100

    copy code to clipboard

---
+++ themes/module/static/js/exercise.js
@@ -0,0 +1,14 @@
+const codeElements = document.querySelectorAll('.chroma:has(code)');
+
+for (const code of codeElements) {
+    const btn = document.createElement("button");
+    btn.textContent = "copy to clipboard";
+    code.parentNode.appendChild(btn);
+    btn.addEventListener('click', ev => {
+        navigator.clipboard.writeText(code.textContent);
+        btn.classList.add('clicked');
+    })
+    btn.addEventListener('transitionend', ev => {
+        btn.classList.remove('clicked');
+    });
+}
