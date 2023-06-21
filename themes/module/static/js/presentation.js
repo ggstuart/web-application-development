f5a672ed7ccc3f1ad05df04bf6511a981a820c1a
Graeme Stuart <ggstuart@gmail.com>
2023-06-21 11:21:28 +0100

    presentation: fix bugs and tidy up

--- themes/module/static/js/presentation.js
+++ themes/module/static/js/presentation.js
@@ -1,14 +1,19 @@
 const slides = Array.from(document.querySelectorAll('.presentation .slide'));
+const firstSlide = document.querySelector(`.presentation .slide:first-of-type`);
+const lastSlide = document.querySelector(`.presentation .slide:last-of-type`);
 
 const params = new URLSearchParams(document.location.search);
+const startSlideNumber = params.has('slide') ? params.get("slide") : 1;
+const startSlide = getSlideByPosition(startSlideNumber);
+activateSlide(startSlide);
 
-if(params.has("slide")) {
-    activateSlide(document.querySelector(`.presentation .slide:nth-of-type(${params.get("slide", 0)})`));
+function getSlideByPosition(position) {
+    const slide = document.querySelector(`.presentation .slide:nth-of-type(${position})`);
+    return slide || document.querySelector(`.presentation .slide:first-of-type`);
 }
 
 function activateSlide(slide) {
-    const slide_number = slides.indexOf(slide) + 1;
-	params.set('slide', slide_number);
+	params.set('slide', slides.indexOf(slide) + 1);
 	window.history.replaceState({}, "", `${window.location.origin}${window.location.pathname}?${params.toString()}`);
     document.querySelectorAll('.presentation .slide.current').forEach(s => s.classList.remove('current'));
     document.querySelectorAll('.presentation .slide.next').forEach(s => s.classList.remove('next'));
@@ -24,17 +29,11 @@ function nextSlide() {
     const current = document.querySelector('.presentation .slide.current')
     const next = current.nextElementSibling || document.querySelector('.presentation .slide:first-of-type');
     activateSlide(next);
-    // current.classList.remove('current');
-    // current.classList.add('left');
-    // next.classList.add("current");
 }
 function prevSlide() {
     const current = document.querySelector('.presentation .slide.current')
     const prev = current.previousElementSibling || document.querySelector('.presentation .slide:last-of-type');
     activateSlide(prev);
-    // current.classList.remove('current');
-    // current.classList.add('right');
-    // prev.classList.add("current");
 }
 
 document.addEventListener('keydown', ev => {
