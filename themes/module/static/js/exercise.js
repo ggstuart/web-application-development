61e9003f031c9860f5d68cad6c707457d016db48
Graeme Stuart <ggstuart@gmail.com>
2023-07-01 19:01:45 +0100

    copy code and code styles

--- themes/module/static/js/exercise.js
+++ themes/module/static/js/exercise.js
@@ -1,14 +1,13 @@
-const codeElements = document.querySelectorAll('.chroma:has(code:is(.language-html, .language-css, .language-js))');
+const codeElements = document.querySelectorAll('pre.chroma:has(code:is(.language-html, .language-css, .language-js))');
 
 for (const code of codeElements) {
+    const highlight = code.closest('.highlight');
     const btn = document.createElement("button");
+    btn.classList.add("copy-to-clipboard")
     btn.textContent = "copy to clipboard";
-    code.parentNode.appendChild(btn);
+    highlight.appendChild(btn);
     btn.addEventListener('click', ev => {
         navigator.clipboard.writeText(code.textContent);
-        btn.classList.add('clicked');
     })
-    btn.addEventListener('transitionend', ev => {
-        btn.classList.remove('clicked');
-    });
 }
+
