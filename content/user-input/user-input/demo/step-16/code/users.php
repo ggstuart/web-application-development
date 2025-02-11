79a2a40ebc2ce1e0dfe7946273b9078ba67e132b
Graeme Stuart <ggstuart@gmail.com>
2025-02-11 08:59:09 +0000

    php example

--- content/user-input/user-input/demo/step-16/code/users.php
+++ content/user-input/user-input/demo/step-16/code/users.php
@@ -1,5 +1,15 @@
 <?php
-    $users = ['Alison', 'Benjamin', 'Carol'];
+    session_start();
+
+    if(!isset($_SESSION['users'])) {
+        $_SESSION['users'] = ['Alison', 'Benjamin', 'Carol'];
+    }
+
+    if (array_key_exists('username', $_POST)) {
+        array_push($_SESSION['users'], $_POST['username']);
+    }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,14 +22,20 @@
 
 <body>
     <?php
-
-    echo in_array('id', $_GET);
-    if (in_array('id', $_GET)) {
-        $selected_user = $users[$$_GET['id']];
+    if (array_key_exists('id', $_GET)) {
+        $selected_user = $_SESSION['users'][$_GET['id']];
         echo "<h2>User {$_GET['id']}</h2>";
         echo "<p>Name: $selected_user</p>";
+        echo "<a href=\"./users.php\">User list</a>";
     } else {
-        var_dump($_GET);
+        echo "<h2>All users</h2>";
+        echo "<ul>";
+        foreach($_SESSION['users'] as $id => $user) {
+            echo "<li><a href=\"?id=$id\">$user</a><form method=\"POST\"><button name=\"delete\">delete</button></form></li>";
+        }
+        echo "</ul>";
+        echo "<form method=\"POST\"><label for=\"username\">New user</label><input id=\"username\" name=\"username\"></form>";
+        var_dump($_SESSION);
     }
     ?>
 </body>
